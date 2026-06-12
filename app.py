cbb2a34eaddcbcd8eeb6ba86d4523ed7fe2e5891
yuvrajbhati649-prog <yuvrajbhati649@gmail.com>
2026-06-12 22:44:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,25 @@
+from dotenv import load_dotenv 
+load_dotenv() 
 
+import streamlit as st 
+import os 
+import google.generativeai as genai 
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+model = genai.GenerativeModel("gemini-2.5-flash") 
+
+def my_output(query):
+    response = model.generate_content(query) 
+    return response.text 
+
+#### UI Development using streamlit 
+
+st.set_page_config(page_title="SMART_BOT")
+st.header("SMART_BOT") 
+input = st.text_input("Input " , key = "input")  
+submit = st.button("Ask your query") 
+
+if submit :
+    response = my_output(input) 
+    st.subheader("The Response is=")
+    st.write(response)
